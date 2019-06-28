fc506f404a31f17b1efca7b87ecbb6e656df286b
Simon Márton <simon.marton@bitrise.io>
2019-06-28 16:42:20 +0200

    Generate minified bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,23 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   input: 'src/main.js',
-  output: [{
-    file: 'bundle-umd.js',
-    format: 'umd',
-    name: 'YmlHelper'
-  },
-  {
-    file: 'bundle-es.js',
-    format: 'es',
-  }],
+  output: [
+    {
+      file: 'dist/bundle-umd.js',
+      format: 'umd',
+      name: 'YmlHelper',
+      compact: true
+    },
+    {
+      file: 'dist/bundle-es.js',
+      format: 'es'
+    }
+  ],
   plugins: [
+    commonjs(),
     resolve(),
     babel({
       exclude: 'node_modules/**'
